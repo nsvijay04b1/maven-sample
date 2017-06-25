17c02c6635c113c3dbf4cb3b82f76a4ca49c6f85
vijaya kumar <vijayakumar.naravula@amdocs.com>
2017-06-25 19:14:07 +0300

    hello git hub chnage

--- testapp/my-webapp/src/main/java/com/mycompany/app/App.java
+++ testapp/my-webapp/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello 2nd app  World!" );
+        System.out.println( "Hello 2nd app github  World!" );
     }
 }
